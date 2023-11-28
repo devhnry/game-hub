34c053ec277786711fedb0f27a07d9003921fd80
devhnry <devwhenry@gmail.com>
2023-11-28 07:19:40 +0100

    Convert App component to use Chakra UI Grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 export default function App() {
   return (
-    <div>
-      <Button colorScheme='blue'>Button</Button>
-    </div>
-  )
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, // 1024px
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="antiquewhite">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
